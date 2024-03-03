86e5c01732546a9f32e5a2201958edf2dca2a136
Bartłomiej Płókarz <bartlomiej.plokarz99@gmail.com>
2024-03-03 16:39:23 +0100

    Sprites, player controller

---
+++ PlayerController.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var move_speed = 300
+
+var hold = false
+
+func _physics_process(delta):
+	move()
+	update_animation()
+	#temp
+	if Input.is_action_just_pressed("interact"):
+		hold = !hold
+
+func move():
+	var move_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = move_direction * move_speed
+	move_and_slide()
+
+func update_animation():
+	var anim_name = ""
+	anim_name += "Run" if velocity != Vector2.ZERO else "Idle"
+	anim_name += "Hold" if hold else ""
+	$AnimationPlayer.play(anim_name)
+	
+	if velocity.x != 0:
+		var flip = velocity.x > 0
+		$Body.flip_h = flip
+		$Body/Arms.flip_h = flip
+		$Body/Eyes.flip_h = flip

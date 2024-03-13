74b4b134e5284262797ae42f022212541be4a81f
Bartłomiej Płókarz <bartlomiej.plokarz99@gmail.com>
2024-03-13 13:00:17 +0100

    Credits screen

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -11,3 +11,6 @@ func _on_play_button_pressed():
 
 func _on_tutorial_button_pressed():
 	get_tree().change_scene_to_file("res://MainMenu/tutorial_screen.tscn")
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://MainMenu/credits_screen.tscn")
